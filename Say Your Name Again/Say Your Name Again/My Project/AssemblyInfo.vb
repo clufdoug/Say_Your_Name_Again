b63e55056f66a3766fd843b410c251b6258fc831
Doug Cluff <clufdoug@isu.edu>
2019-09-01 15:15:49 -0600

    Created .vb and .exe

---
+++ Say Your Name Again/Say Your Name Again/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Say Your Name Again")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Say Your Name Again")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1459d5e7-e713-46f7-93c0-f17a0996d6d9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Say_Your_Name_Again.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Do
+            Console.WriteLine("What is your name?")
+            Console.Write("Type your first name ")
+            Dim firstname As String
+            firstname = Console.ReadLine()
+
+            Dim response As String = ""
+            If firstname = "Emily" Then
+                response = "Hello Emily, Your hair looks nice"
+            ElseIf firstname = "Joe" Then
+                response = "Dammit Joe, Tie your shoes!!"
+            ElseIf firstname = "Doug" Then
+                response = "Doug is a steely-eyed missle man."
+            End If
+
+            Console.WriteLine(response)
+        Loop
+    End Sub
+
+End Module
